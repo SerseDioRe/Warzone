90f4baf9e325984be13d5056b06dfa3b50143e8a
Serse Dio Re <32899592+SerseDioRe@users.noreply.github.com>
2021-12-10 21:21:01 +0100

    offset recoil updated

--- Warzone/Macro.h
+++ Warzone/Macro.h
@@ -4,7 +4,7 @@
 #define ADDRESS_CG_T 0x1E67AEB0                           // [[48 63 C1 48 69 C8 60 03 00 00 48 8D ? ? ? ? ? 48 03 C8 E9] + 0xA] lea - 0x18
 #define ADDRESS_GAMEMODE 0x1E1E9F08                       // [74 1D 48 8B 8B ? ? ? ? 48 2B 8E ? ? ? ? 2B 05 ? ? ? ?] - 0x17                     2B 05 6E3F9A13        - sub eax,[ADDRESS_GAMEMODE]
 #define ADDRESS_CHECK_UPDATE 0x41146DF                    // 74 1D 48 8B 8B ? ? ? ? 48 2B 8E ? ? ? ? 2B 05 ? ? ? ?
-#define ADDRESS_RECOIL 0x59548                            // [[41 8B 52 0C 4D 8D 4A 04 4D 8D 42 08 4C 89 95 ? ? ? ? 8B C2 4C 89 8D] - 0x9] [0F 85 ? ? ? ? 0F 2E 80 ? ? ? ? 0F 85 ? ? ? ? 4C 8D 96 ? ? ? ?]
+#define ADDRESS_RECOIL 0x59548                            // [4C 8D 9E ? ? ? ? B3 ?] [[41 8B 52 0C 4D 8D 4A 04 4D 8D 42 08 4C 89 95 ? ? ? ? 8B C2 4C 89 8D] - 0x9] [0F 85 ? ? ? ? 0F 2E 80 ? ? ? ? 0F 85 ? ? ? ? 4C 8D 96 ? ? ? ?]
 
 //Options
 #define KEY_MODULE_EJECT GetAsyncKeyState(VK_END) & 1
